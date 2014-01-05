b7cdce60ae9d24cb52cccf7e2e7572aa4f2920e4
Alon Niv <oakfang@gmail.com>
2014-01-05 18:59:42 +0200

    added init files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cain',
+    version='0.1',
+    packages=['cain'],
+    url='',
+    license='',
+    author='oakfang',
+    author_email='',
+    description='A framework for lightweight mixins.'
+)
